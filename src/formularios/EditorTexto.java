9ab59cc8508dbab816595921899656d51432a9e1
Miguel Monje <mmv776@alumnos.unican.es>
2023-11-23 19:05:11 +0100

    Añadido metodos de control de texto implementado menu de Archivo y Edicion

--- src/formularios/EditorTexto.java
+++ src/formularios/EditorTexto.java
@@ -12,6 +12,8 @@ import javax.swing.JMenu;
 import javax.swing.JMenuItem;
 import javax.swing.KeyStroke;
 import java.awt.event.KeyEvent;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.InputEvent;
 import javax.swing.JTextPane;
 import javax.swing.JSeparator;
@@ -20,11 +22,19 @@ import javax.swing.DefaultComboBoxModel;
 import javax.swing.JButton;
 import javax.swing.ImageIcon;
 import javax.swing.SwingConstants;
+import javax.swing.text.SimpleAttributeSet;
+import javax.swing.text.StyleConstants;
+import javax.swing.text.StyledDocument;
 
-public class EditorTexto {
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import javax.swing.JLabel;
+
+public class EditorTexto implements ActionListener {
 
 	private JFrame frame;
 	private JTextPane textPane;
+	private JLabel lblBarraEstado;
 
 	/**
 	 * Launch the application.
@@ -69,30 +79,42 @@ public class EditorTexto {
 		menuBar.add(mnArchivo);
 		
 		JMenuItem mntmNuevo = new JMenuItem("Nuevo");
-		mntmNuevo.setIcon(null);
 		mntmNuevo.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_N, InputEvent.CTRL_DOWN_MASK));
+		mntmNuevo.addActionListener(this);
 		mnArchivo.add(mntmNuevo);
 		
 		JMenuItem mntmAbrir = new JMenuItem("Abrir");
-		mntmAbrir.setIcon(null);
+		mntmAbrir.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_O, InputEvent.CTRL_DOWN_MASK));
+		mntmAbrir.addActionListener(this);
 		mnArchivo.add(mntmAbrir);
 		
 		JMenuItem mntmGuardar = new JMenuItem("Guardar");
+		mntmGuardar.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S, InputEvent.CTRL_DOWN_MASK));
+		mntmGuardar.addActionListener(this);
 		mnArchivo.add(mntmGuardar);
 		
 		JMenuItem mntmCerrar = new JMenuItem("Cerrar");
+		mntmCerrar.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_W, InputEvent.CTRL_DOWN_MASK));
+		mntmCerrar.addActionListener(this);
 		mnArchivo.add(mntmCerrar);
-		
+	
 		JMenu mnEdicion = new JMenu("Edición");
+		mnEdicion.setMnemonic('E');
 		menuBar.add(mnEdicion);
 		
 		JMenuItem mntmCopiar = new JMenuItem("Copiar");
+		mntmCopiar.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_C, InputEvent.CTRL_DOWN_MASK));
+		mntmCopiar.addActionListener(this);
 		mnEdicion.add(mntmCopiar);
 		
 		JMenuItem mntmCortar = new JMenuItem("Cortar");
+		mntmCortar.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_X, InputEvent.CTRL_DOWN_MASK));
+		mntmCortar.addActionListener(this);
 		mnEdicion.add(mntmCortar);
 		
 		JMenuItem mntmPegar = new JMenuItem("Pegar");
+		mntmPegar.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_V, InputEvent.CTRL_DOWN_MASK));
+		mntmPegar.addActionListener(this);
 		mnEdicion.add(mntmPegar);
 		
 		JSeparator separator = new JSeparator();
@@ -102,24 +124,30 @@ public class EditorTexto {
 		mnEdicion.add(mnAlinear);
 		
 		JMenuItem mntmAlinearIzquierda = new JMenuItem("Izquierda");
+		mntmAlinearIzquierda.addActionListener(this);
 		mnAlinear.add(mntmAlinearIzquierda);
 		
 		JMenuItem mntmAlinearDerecha = new JMenuItem("Derecha");
+		mntmAlinearDerecha.addActionListener(this);
 		mnAlinear.add(mntmAlinearDerecha);
 		
 		JMenuItem mntmAlinearCentrada = new JMenuItem("Centrada");
+		mntmAlinearCentrada.addActionListener(this);
 		mnAlinear.add(mntmAlinearCentrada);
 		
 		JMenuItem mntmAlinearJustificada = new JMenuItem("Justificada");
+		mntmAlinearJustificada.addActionListener(this);
 		mnAlinear.add(mntmAlinearJustificada);
 		
 		JSeparator separator_1 = new JSeparator();
 		mnEdicion.add(separator_1);
 		
 		JMenuItem mntmBuscarTexto = new JMenuItem("Buscar");
+		mntmBuscarTexto.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_F, InputEvent.CTRL_DOWN_MASK));
 		mnEdicion.add(mntmBuscarTexto);
 		
 		JMenu mnFormato = new JMenu("Formato");
+		mnFormato.setMnemonic('F');
 		menuBar.add(mnFormato);
 		
 		JMenu mnNewMenu_1 = new JMenu("Tamaño");
@@ -197,9 +225,115 @@ public class EditorTexto {
 		
 		textPane = new JTextPane();
 		panelTextArea.add(textPane);
+		
+		lblBarraEstado = new JLabel("Editor de texto abierto");
+		panelTextArea.add(lblBarraEstado, BorderLayout.SOUTH);
 	}
 	
-	private void limpiarEditorTexto() {
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		String s = e.getActionCommand();
+		
+		switch(s)
+		{
+			case "Nuevo":
+				nuevoDocumento();
+				break;
+			case "Abrir":
+				abrirDocumento();
+				break;
+			case "Guardar":
+				guardarDocumento();
+				break;
+			case "Cerrar":
+				cerrarDocumento();
+				break;
+			case "Copiar":
+				copiarTexto();
+				break;
+			case "Cortar":
+				cortarTexto();
+				break;
+			case "Pegar":
+				pegarTexto();
+				break;
+			case "Izquierda":
+				alinearIzquierda();
+				break;
+			case "Derecha":
+				alinearDerecha();
+				break;
+			case "Centrada":
+				alinearCentrada();
+				break;
+			case "Justificada":
+				alinearJustificada();
+				break;
+			default:
+		}
+	}
+	
+	private void nuevoDocumento() {
+		lblBarraEstado.setText("Creado nuevo documento.");
 		textPane.setText("");
+	}
+	private void abrirDocumento() {
+		lblBarraEstado.setText("Abierto documento.");
+		textPane.setText("");
+	}
+	private void guardarDocumento() {
+		lblBarraEstado.setText("Documento guardado.");
+	}
+	private void cerrarDocumento() {
+		lblBarraEstado.setText("Documento cerrado");
+		textPane.setText("");
+	}
+	private void copiarTexto() {
+		lblBarraEstado.setText("Texto copiado");
+		textPane.copy();
+	}
+	private void cortarTexto() {
+		lblBarraEstado.setText("Texto cortado");
+		textPane.cut();
+	}
+	private void pegarTexto() {
+		lblBarraEstado.setText("Texto pegado");
+		textPane.paste();
+	}
+	private void alinearIzquierda() {
+		SimpleAttributeSet atributos = new SimpleAttributeSet();
+		StyleConstants.setAlignment(atributos, StyleConstants.ALIGN_LEFT);
+		actualizarParrafo(atributos);
+		lblBarraEstado.setText("Parrafo alineado izquierda");
+	}
+	private void alinearDerecha() {
+		SimpleAttributeSet atributos = new SimpleAttributeSet();
+		StyleConstants.setAlignment(atributos, StyleConstants.ALIGN_RIGHT);
+		actualizarParrafo(atributos);
+		lblBarraEstado.setText("Parrafo alineado derecha");
+	}
+	private void alinearCentrada() {
+		SimpleAttributeSet atributos = new SimpleAttributeSet();
+		StyleConstants.setAlignment(atributos, StyleConstants.ALIGN_CENTER);
+		actualizarParrafo(atributos);
+		lblBarraEstado.setText("Parrafo alineado centrado");
+	}
+	private void alinearJustificada() {
+		SimpleAttributeSet atributos = new SimpleAttributeSet();
+		StyleConstants.setAlignment(atributos, StyleConstants.ALIGN_JUSTIFIED);
+		actualizarParrafo(atributos);
+		lblBarraEstado.setText("Parrafo alineado justificado");
+	}
+	private void actualizarCaracteres(SimpleAttributeSet atributos) {
+		int inicioSeleccion = textPane.getSelectionStart();
+		int finSeleccion = textPane.getSelectionEnd();
+		int longitudSeleccion = finSeleccion - inicioSeleccion;
+		textPane.getStyledDocument().setCharacterAttributes(inicioSeleccion, longitudSeleccion, atributos, false);
+	}
+	private void actualizarParrafo(SimpleAttributeSet atributos) {
+		int inicioSeleccion = textPane.getSelectionStart();
+		int finSeleccion = textPane.getSelectionEnd();
+		int longitudSeleccion = finSeleccion - inicioSeleccion;
+		textPane.getStyledDocument().setParagraphAttributes(inicioSeleccion, longitudSeleccion, atributos, false);
 	}
 }
